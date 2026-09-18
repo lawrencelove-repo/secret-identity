57d9a443179eb5376d24e1692024b024afae639d
Lawrence Love <lawrencelove@gmail.com>
2026-09-18 10:52:16 -0500

    Round indicator tweak

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Minimal offline cache for Secret Identity PWA */
-const CACHE_NAME = "secret-identity-v30";
+const CACHE_NAME = "secret-identity-v31";
 const ASSETS = [
   "./",
   "./index.html",
