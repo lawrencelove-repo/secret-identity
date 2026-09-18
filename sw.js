7aaa0eeb46109c5e14067bebd962c7cf0d3f2b78
Lawrence Love <lawrencelove@gmail.com>
2026-09-18 10:08:07 -0500

    Adding hamburger button to board module

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Minimal offline cache for Secret Identity PWA */
-const CACHE_NAME = "secret-identity-v27";
+const CACHE_NAME = "secret-identity-v28";
 const ASSETS = [
   "./",
   "./index.html",
