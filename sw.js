aae8812434a470a91ba431c394c6358faf905ff9
Lawrence Love <lawrencelove@gmail.com>
2026-09-19 09:50:36 -0500

    Focus tweak

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Minimal offline cache for Secret Identity PWA */
-const CACHE_NAME = "secret-identity-v37";
+const CACHE_NAME = "secret-identity-v38";
 const ASSETS = [
   "./",
   "./index.html",
