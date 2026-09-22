36fb8a3cff3e339e6a69f63ad92918f76522211a
Lawrence Love <lawrencelove@gmail.com>
2026-09-22 13:14:29 -0500

    Logo size adjustment

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Minimal offline cache for Secret Identity PWA */
-const CACHE_NAME = "secret-identity-v49";
+const CACHE_NAME = "secret-identity-v50";
 const ASSETS = [
   "./",
   "./index.html",
